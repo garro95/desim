de9d1289876339a5d42dcf3aaa1de5ab11ee9af7
Gianmarco <gianmarcoesgarix@gmail.com>
2018-03-30 00:58:24 +0200

    Added copyright

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+/* Copyright © 2018 Gianmarco Garrisi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 //! This crate implement a discrete event simulation framework
 //! inspired by the SimPy library for Python. It uses the generator
 //! feature that is nightly. Once the feature is stabilized, also this
